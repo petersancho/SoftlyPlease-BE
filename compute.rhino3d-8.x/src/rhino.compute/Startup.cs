62a9865dcbe4db0c3a58e56d6cdefa6bbf2aa9d8
SoftlyPlease Compute <user@softlyplease.com>
2025-08-24 16:49:19 +0000

    Initial commit - SoftlyPlease Compute AppServer setup

---
+++ compute.rhino3d-8.x/src/rhino.compute/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace rhino.compute
+{
+    using System;
+    using Carter;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+    using Serilog;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyHeader();
+                    });
+            });
+            services.AddHealthChecks();
+            services.AddCarter();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseSerilogRequestLogging();
+            app.UseRouting();
+            app.UseCors();
+            if (!String.IsNullOrEmpty(Config.ApiKey))
+                app.UseMiddleware<ApiKeyMiddleware>();
+            app.UseEndpoints(builder =>
+            {
+                builder.MapHealthChecks("/healthcheck");
+                builder.MapCarter();
+            });
+        }
+    }
+}
