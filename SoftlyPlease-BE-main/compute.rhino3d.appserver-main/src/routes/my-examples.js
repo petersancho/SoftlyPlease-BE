1852e6e607bd64622dfd27ba481696ac8f7b1ebf
petersancho <petersancho@example.com>
2025-08-27 23:22:22 +0000

    Update 'Topological Optimization' link on 'My Examples' page to point to .gh file.

--- SoftlyPlease-BE-main/compute.rhino3d.appserver-main/src/routes/my-examples.js
+++ SoftlyPlease-BE-main/compute.rhino3d.appserver-main/src/routes/my-examples.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/', function(req, res, next) {
     const myExamples = [
-        { name: "Topological Optimization", url: "/my-examples/topological-optimization/index.html" }
+        { name: "Topological Optimization", url: "/files/topological-optimization.gh" }
         // Add other custom examples here as needed
     ];
     res.render('my-examples', { title: 'My Examples', examples: myExamples });
